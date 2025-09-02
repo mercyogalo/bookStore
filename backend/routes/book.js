a36bdbaede351013164b8caf1e3193c3a8646f0b
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-09-02 23:34:53 +0300

    Added the book route to delete book

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,18 +3,22 @@ const Book = require("../models/Book");
 const router = express.Router();
 const generateToken = require("../Utils/token");
 const protect=require('../middlewares/auth');
+const checkRole=require('../middlewares/role');
 
 
 
 
-router.post('/createBook', protect, async (req,res)=>{
+router.post('/createBook', protect,checkRole(["author"]), async (req,res)=>{
     const { title, author, yearPublished, link, description, coverImage, genre}=req.body;
     try {
+        if(req.user.role!=="author"){
+            res.status(403).json({message:"Only authors can add books"})
+        }
          if(!title || !author || !link || !description){
             res.status(400).json({message:"Please enter all fields"});
          }
 
-         const book=await Book.create({ title, author, yearPublished, link, description, coverImage, genre});
+         const book=await Book.create({ title, author, yearPublished, link, description, coverImage, genre, createdBy: req.user._id });
          res.status(200).json({message:"Book added successfully"});
     } catch (error) {
         console.error(error);
@@ -25,14 +29,52 @@ router.post('/createBook', protect, async (req,res)=>{
 })
 
 
-router.put('/updateBook:id', protect, async (req,res)=>{
+router.put('/updateBook:id', protect,checkRole(["author"]) ,async (req,res)=>{
+    try {
+        const book=await Book.findById(req.params.id);
 
-    
+        if(!book){
+            res.status(404).json({message:"Book not found"});
+        }
+
+        if(req.user.role=="author" && book.createdBy.toString()== !req.user._id.toString()){
+            res.status(403).json({message:"You can only update your own books"});
+        }
+
+        Object.assign(book, req.body);
+        const updateBook=await book.save();
+
+        res.status(200).json({message:"Book details updated successfully"});
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:"Server error try again"});
+    }
 })
 
 
-router.delete('/deleteBook:id', protect, async (req,res)=>{
-    
+router.delete('/deleteBook:id', protect,checkRole(["author"]),  async (req,res)=>{
+    try {
+
+        const book=await Book.findById(req.params.id);
+
+        if(!book){
+            res.status(404).json({message:"Book not found"});
+        }
+
+        if(req.user.role=="author" && book.createdBy.toString()== !req.user._id.toString()){
+            res.status(403).json({message:"You can only delete your own books"});
+        }
+
+        await book.deleteOne();
+
+        res.status(200).json({message:"Book deleted successfully"});
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:"Server error try again"});
+    }
 })
 
 module.exports=router;
